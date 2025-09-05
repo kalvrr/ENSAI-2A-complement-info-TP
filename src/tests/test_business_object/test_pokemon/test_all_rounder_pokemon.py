945ce9a6ac12d2603bff749e99553ee9f4c2c4c4
kalvr <claire.robin@eleve.ensai.fr>
2025-09-05 10:15:36 +0000

    ajout des tests et init

---
+++ src/tests/test_business_object/test_pokemon/test_all_rounder_pokemon.py
@@ -0,0 +1,7 @@
+from business_object.pokemon.all_rounder_pokemon import AllRounderPokemon
+from business_object.statistic import Statistic
+
+class TestAllRounderPokemon:
+
+    def lalala():
+        pass
---
+++ src/tests/test_business_object/test_pokemon/test_attacker_pokemon.py
@@ -0,0 +1,6 @@
+from business_object.pokemon.attacker_pokemon import AttackerPokemon
+from business_object.statistic import Statistic
+
+class TestAttackerPokemon:
+    
+    pass
