7ea1995e0c936107cc61191088d04d3f6c35ae6a
kalvr <claire.robin@eleve.ensai.fr>
2025-09-05 09:55:09 +0000

    test

--- src/business_object/pokemon/all_rounder_pokemon.py
+++ src/business_object/pokemon/all_rounder_pokemon.py
@@ -1,10 +1,8 @@
-    def get_pokemon_attack_coef(self) -> float:
-        """
-        Compute a damage multiplier related to the pokemon type.
+from business_object.pokemon.abstract_pokemon import AbstractPokemon
 
-        Returns :
-            float : the multiplier
-        """
-        elif self._type == "All rounder":
-            multiplier = 1 + (self.sp_atk_current + self.sp_def_current) / 200
+
+class AllRounderPokemon(AbstractPokemon):
+
+    def get_pokemon_attack_coef(self) -> float:
+        multiplier = 1 + (self.sp_atk_current + self.sp_def_current) / 200
         return multiplier
--- src/business_object/pokemon/attacker_pokemon.py
+++ src/business_object/pokemon/attacker_pokemon.py
@@ -4,11 +4,5 @@ from business_object.pokemon.abstract_pokemon import AbstractPokemon
 class AttackerPokemon(AbstractPokemon):
 
     def get_pokemon_attack_coef(self) -> float:
-        """
-        Compute a damage multiplier related to the pokemon type.
-
-        Returns :
-            float : the multiplier
-        """
         multiplier = 1 + (self.speed_current + self.attack_current) / 200
         return multiplier
